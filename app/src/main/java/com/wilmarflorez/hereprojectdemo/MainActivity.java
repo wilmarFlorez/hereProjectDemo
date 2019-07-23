121d65a4d21b427e6a50a04bddc8286fe3033d0f
Wilmar Florez <wflorez52@gmail.com>
2019-07-23 15:47:19 -0500

    map created and mark added

--- app/src/main/java/com/wilmarflorez/hereprojectdemo/MainActivity.java
+++ app/src/main/java/com/wilmarflorez/hereprojectdemo/MainActivity.java
@@ -1,47 +1,145 @@
 package com.wilmarflorez.hereprojectdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.Image;
 import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.common.PositioningManager;
 import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.mapping.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
+public class MainActivity extends FragmentActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    // permissions request code
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+
+    private Image markImage = new Image();
+
+    public static final double LAT = 6.246283;
+    public static final double LNG = -75.575202;
+
+    // Set this to PositioningManager.getInstance() upon Engine Initialization
+    private PositioningManager posManager;
+
+
+
+    /**
+     * Permissions that need to be explicitly requested from end user.
+     */
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+    // map embedded in the map fragment
     private Map map = null;
+
+    // map fragment embedded in this activity
     private SupportMapFragment mapFragment = null;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initialize();
+        checkPermissions();
+    }
 
+    private SupportMapFragment getSupportMapFragment() {
+        return (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
     }
 
     private void initialize() {
         setContentView(R.layout.activity_main);
+
         // Search for the map fragment to finish setup by calling init().
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
+        mapFragment = getSupportMapFragment();
+
         mapFragment.init(new OnEngineInitListener() {
             @Override
             public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
                 if (error == OnEngineInitListener.Error.NONE) {
+
                     // retrieve a reference of the map from the map fragment
                     map = mapFragment.getMap();
                     // Set the map center to the Vancouver region (no animation)
-                    map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
+                    map.setCenter(new GeoCoordinate(LAT, LNG, 0.0),
                             Map.Animation.NONE);
+                    try{
+                        markImage.setImageResource(R.drawable.mark);
+                    }catch (IOException e){
+                        finish();
+                    }
+
+                    MapMarker myMapMarker = new MapMarker(new GeoCoordinate(LAT, LNG), markImage);
+                    map.addMapObject(myMapMarker);
+
                     // Set the zoom level to the average between min and max
                     map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
                 } else {
-                    System.out.println("ERROR: Cannot initialize Map Fragment");
+                    Log.e(LOG_TAG, "Cannot initialize SupportMapFragment (" + error + ")");
                 }
             }
         });
     }
+
+    /**
+     * Checks the dynamically controlled permissions and requests missing permissions from end user.
+     */
+    protected void checkPermissions() {
+        final List<String> missingPermissions = new ArrayList<String>();
+        // check all required dynamic permissions
+        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission);
+            }
+        }
+        if (!missingPermissions.isEmpty()) {
+            // request all missing permissions
+            final String[] permissions = missingPermissions
+                    .toArray(new String[missingPermissions.size()]);
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
+        } else {
+            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
+            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
+            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
+                    grantResults);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                for (int index = permissions.length - 1; index >= 0; --index) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                        // exit the app if one permission is not granted
+                        Toast.makeText(this, "Required permission '" + permissions[index]
+                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                // all permissions were granted
+                initialize();
+                break;
+        }
+    }
+
 }
